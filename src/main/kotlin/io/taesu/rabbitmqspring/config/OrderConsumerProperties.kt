5f6b492b9b4ac2de57b44f5ea797c89f1454611a
taesu <taesu@crscube.co.kr>
2024-07-18 14:31:42 +0900

    [Orders] Config 추가

---
+++ src/main/kotlin/io/taesu/rabbitmqspring/config/OrderConsumerProperties.kt
@@ -0,0 +1,17 @@
+package io.taesu.rabbitmqspring.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.bind.ConstructorBinding
+
+/**
+ * Created by itaesu on 2024. 7. 17..
+ *
+ * @author Lee Tae Su
+ * @version rabbitmq-spring
+ * @since rabbitmq-spring
+ */
+@ConfigurationProperties("app.orders.consumer")
+class OrderConsumerProperties @ConstructorBinding constructor(
+    val exchange: String,
+    val instances: List<ConsumerProperty>,
+)
---
+++ src/main/kotlin/io/taesu/rabbitmqspring/config/OrdersConsumerConfig.kt
@@ -0,0 +1,74 @@
+package io.taesu.rabbitmqspring.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.amqp.core.*
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by itaesu on 2024. 7. 17..
+ *
+ * @author Lee Tae Su
+ * @version rabbitmq-spring
+ * @since rabbitmq-spring
+ */
+@Configuration
+class OrdersConsumerConfig(private val properties: OrderConsumerProperties) {
+    @Bean
+    fun ordersDeclarables(): Declarables {
+        val exchange = DirectExchange(properties.exchange, true, false)
+        val queues = properties.instances.map {
+            it.routingKey to Queue(it.topic, true, false, false).apply {
+                it.dlRoutingKey ?: return@apply
+                arguments["x-dead-letter-exchange"] = properties.exchange   // DLX
+                arguments["x-dead-letter-routing-key"] = it.dlRoutingKey    // DLQ
+            }
+        }
+
+        val dlQueues = properties.instances.mapNotNull {
+            it.dlTopic ?: return@mapNotNull null
+            it.dlRoutingKey ?: return@mapNotNull null
+            it.dlRoutingKey to Queue(it.dlTopic, true, false, false)
+        }
+        val queueMap = (queues + dlQueues).toMap()
+
+        val queueBindings = properties.instances.mapNotNull {
+            val queue = queueMap[it.routingKey] ?: return@mapNotNull null
+            BindingBuilder.bind(queue)
+                .to(exchange)
+                .with(it.routingKey)
+        }
+
+        val deadLetterQueueBindings = properties.instances.mapNotNull {
+            val queue = queueMap[it.dlRoutingKey] ?: return@mapNotNull null
+            BindingBuilder.bind(queue)
+                .to(exchange)
+                .with(it.dlRoutingKey)
+        }
+
+        return Declarables(
+            exchange,
+            *queueMap.values.toTypedArray(),
+            *(queueBindings + deadLetterQueueBindings).toTypedArray()
+        )
+    }
+
+    @Bean
+    fun rabbitListenerContainerFactory(
+        configurer: SimpleRabbitListenerContainerFactoryConfigurer,
+        connectionFactory: ConnectionFactory,
+        objectMapper: ObjectMapper,
+    ): SimpleRabbitListenerContainerFactory {
+        return SimpleRabbitListenerContainerFactory().apply {
+            configurer.configure(this, connectionFactory)
+            this.setMessageConverter(Jackson2JsonMessageConverter(objectMapper))
+            this.setAfterReceivePostProcessors(GlobalConsumerMessagePostProcessor())
+            this.setAcknowledgeMode(AcknowledgeMode.MANUAL)
+            this.setPrefetchCount(1)
+        }
+    }
+}
